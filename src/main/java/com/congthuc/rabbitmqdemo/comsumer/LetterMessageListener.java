922897ea30d9c447f39290a20a26ade4d2fcc70b
Thuc Phan <congthuc.uit@gmail.com>
2019-05-10 11:04:42 +0700

    handling for failure process message

--- src/main/java/com/congthuc/rabbitmqdemo/comsumer/LetterMessageListener.java
+++ src/main/java/com/congthuc/rabbitmqdemo/comsumer/LetterMessageListener.java
@@ -3,8 +3,10 @@ package com.congthuc.rabbitmqdemo.comsumer;
 import com.congthuc.rabbitmqdemo.config.RabbitConfig;
 import com.congthuc.rabbitmqdemo.dto.Letter;
 import com.congthuc.rabbitmqdemo.dto.Order;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,9 @@ public class LetterMessageListener {
 
     @RabbitListener(queues = RabbitConfig.QUEUE_LETTERS)
     public void processLetter(Letter letter) {
+        if(StringUtils.isEmpty(letter.getSubject())) {
+            throw new AmqpRejectAndDontRequeueException("Could not process the letter missed the subject");
+        }
         logger.info("Letter Received: " + letter);
     }
 }
--- src/main/java/com/congthuc/rabbitmqdemo/comsumer/OrderMessageListener.java
+++ src/main/java/com/congthuc/rabbitmqdemo/comsumer/OrderMessageListener.java
@@ -4,6 +4,7 @@ import com.congthuc.rabbitmqdemo.config.RabbitConfig;
 import com.congthuc.rabbitmqdemo.dto.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,10 @@ public class OrderMessageListener {
     static final Logger logger = LoggerFactory.getLogger(OrderMessageListener.class);
 
     @RabbitListener(queues = RabbitConfig.QUEUE_ORDERS)
-    public void processOrder(Order order) {
+    public void processOrder(Order order) throws Exception {
+        if(order.getAmount() > 2000) {
+            throw new AmqpRejectAndDontRequeueException("Could not process the order that greater than 3000$");
+        }
         logger.info("Order Received: " + order);
     }
 }
